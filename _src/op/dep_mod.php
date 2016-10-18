19099b9f6dd98412dbf52e085f8f4180a933f163
Illuminati Insider <oranje.db@gmail.com>
2016-10-18 21:12:56 +0300

    Update dep_mod.php

--- _src/op/dep_mod.php
+++ _src/op/dep_mod.php
@@ -1,53 +1,52 @@
 ﻿<?php 	
-switch ($data['type']){
-	case 'add':
-		$name = check_str($data['name']);
-		$query = "INSERT INTO `Departments` (`Name`)
-			VALUES ('$name');";		
-		if($mysql->query($query)) {
-			$output = array('id' => $mysql->insert_id, 'name' => $name);
+switch ($data['type']){ 								// выбор между add, list, delete, modify
+	case 'add': 									// добавление 
+		$name = check_str($data['name']); 					// берем из массива $data name
+		$query = "INSERT INTO `Departments` (`Name`) 
+			VALUES ('$name');";						// запрос на добавление
+		if($mysql->query($query)) { 						// проверка на выполнение и выполнение запроса
+			$output = array('id' => $mysql->insert_id, 'name' => $name); 	// Добавляем в массив output нужные данные
 		} 			
 		else 	{
-			throw403();
+			throw403();							 // ошибка
 		}				
 		break;
 				
-	case 'list':
+	case 'list': 									// показать 
 		$query = "-- Получение списка аудиторий
-			SELECT `ID`, `name` FROM `Departments`;";
-		if ($result = $mysql->query($query)) {	
-			$output = array();
-			while ($row = $result->fetch_row())  {
+			SELECT `ID`, `name` FROM `Departments`;"; 			// запрос на показ
+		if ($result = $mysql->query($query)) {					// проверка на выполнение и выполнение запроса
+			$output = array(); 						// иниц. массива 
+			while ($row = $result->fetch_row())  { 				// выбираем 
 				$output[] = array(
-					'id' => $row[0],
-					'name' => $row[1] 
+					'id' => $row[0], 				// id
+					'name' => $row[1] 				// наименование
 				);						
 			}								
 		} 	
 		else {
-			throw403();
+			throw403(); // ошибка
 		} 	
 		break;
-	case 'modify':
-		$new_name=check_str($data['name']);
-		$ID = checkInt($data['depID']);
-		$query = "-- Изменение данных аудитории
+	case 'modify': // изменение
+		$new_name=check_str($data['name']); //берем из массива имя
+		$ID = checkInt($data['depID']);// берем из массива ID
+		$query = "-- Изменение данных аудитории 
 			UPDATE `Departments` 
 			SET `name` = '$new_name' , `Modified` = CURRENT_TIMESTAMP 
-			WHERE `id` = $ID;";							
-		if(!($mysql->query($query))) {								
+			WHERE `id` = $ID;";	// запрос на измененение						
+		if(!($mysql->query($query))) {	// проверка на невыполнение, если да, то ошибка							
 			throw403();
 		} 
-		$output = array('id' => $ID, 'name' => $new_name);								
+		$output = array('id' => $ID, 'name' => $new_name);	// добавляем в массив данные							
 		break;
-				
-				
-	case 'delete':
-		$ID = checkInt($data['depID']);
-		$query = "DELETE FROM `departments` WHERE `departments`.`ID` = $ID;
-			INSERT INTO `dellog` (`Text`, `ID`) VALUES ('dep', $ID);";
-		runMultiQuery($query); 					
-		$output = array('id' =>$ID);						
+					
+	case 'delete': // удаление
+		$ID = checkInt($data['depID']); // ID
+		$query = "DELETE FROM `departments` WHERE `departments`.`ID` = $ID; 
+			INSERT INTO `dellog` (`Text`, `ID`) VALUES ('dep', $ID);";// запрос на удаление
+		runMultiQuery($query); 		// запускаем функцию выполнения мультизапроса			
+		$output = array('id' =>$ID);	// добавляем в массив ID					
 		break;	 
 }
 		?>
