9ae024e9273a2015432f53aff52ddd69f1724210
Illuminati Insider <oranje.db@gmail.com>
2016-10-18 22:03:04 +0300

    Update mark_mod.php

--- _src/op/mark_mod.php
+++ _src/op/mark_mod.php
@@ -2,15 +2,15 @@
 switch ($data['type']){
 	case 'add': 												// добавление
 		/* Блок взятия переменных из data*/
-		$mark = check_str($data['mark']);
-		$studentID = checkInt($data['studentID']);			
-		$programID = checkInt(data['programID']);
-		$profID =  checkInt($data['profID']);
+		$mark = check_str($data['mark']); 								// взятие из data оценки
+		$studentID = checkInt($data['studentID']); 							// взятие из data ID студента			
+		$programID = checkInt(data['programID']); 							// взятие из data ID программы
+		$profID =  checkInt($data['profID']); 								// взятие из data ID препода
 		/* ////////////////// */
 		$query = "-- Добавление оценки
 			INSERT INTO `StudentResults` (`Students_ID`, `Programs_ID`, `Profs_ID`, `Mark`,`Modified`)
 			VALUES ($studentID, $programID, $profID, '$mark',CURRENT_TIMESTAMP);"; 			// запрос на добавление
-		if($mysql->query($query)) {
+		if($mysql->query($query)) { 									// проверка на выполнение запроса и выполнение запроса
 			$output = array('ID' => $mysql->insert_id, 'studentID' => $studentID, 'programID' => $programID, 'profID' => $profID, 'mark' => $mark);
 		} 			
 		else {
@@ -19,12 +19,12 @@ switch ($data['type']){
 		break;
 		
 	case 'list':
-		$studentID = checkInt($data['studentID']);
+		$studentID = checkInt($data['studentID']); // взятие из data ID студента 
 		$query = "SELECT `ID`, `Programs_ID`, `Profs_ID`, `Mark` 
 			FROM `StudentResults` 
-			WHERE `Students_ID` = $studentID;"; // запрос на показать 
-		if ($result = $mysql->query($query)) {	
-			$output = array();
+			WHERE `Students_ID` = $studentID;"; 							// запрос на показать 
+		if ($result = $mysql->query($query)) {								// проверка на выполнение запроса и выполнение запр
+			$output = array(); 									// иниц массива
 			while ($row = $result->fetch_row()) {
 				$output[] = array(
 					'id' => $row[0],
@@ -34,15 +34,15 @@ switch ($data['type']){
 					);		
 			}	
 		} else {
-			throw403();
+			throw403(); 										// ошибка
 		} 	
 		break;	
 		
 	case 'delete':
 		$noteID = checkInt($data['noteID']);
 		$query = "DELETE FROM `StudentResults` WHERE `ID` = $noteID;
-			INSERT INTO `dellog` (`Text`, `ID`) VALUES ('mark', $noteID);";
-		runMultiQuery($query); // запуск мультизапроса
+			INSERT INTO `dellog` (`Text`, `ID`) VALUES ('mark', $noteID);"; 			// запрос на удаление
+		runMultiQuery($query); 										// запуск мультизапроса
 		$output = array('ID' =>$noteID);	
 		break; 
 		}
