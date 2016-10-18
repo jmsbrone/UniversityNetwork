8921a0a837a579aa004f494473e093f5504100a1
Illuminati Insider <oranje.db@gmail.com>
2016-10-18 21:39:01 +0300

    Update mark_mod.php

--- _src/op/mark_mod.php
+++ _src/op/mark_mod.php
@@ -1,50 +1,49 @@
-﻿<?php switch ($data['type']){
-		case 'add':
-			$mark = check_str($data['mark']);
-			$studentID = checkInt($data['studentID']);
-			$programID = checkInt(data['programID']);
-			$profID =  checkInt($data['profID']);
-			$query = "-- Добавление оценки
-						INSERT INTO `StudentResults` (`Students_ID`, `Programs_ID`, `Profs_ID`, `Mark`,`Modified`)
-						VALUES ($studentID, $programID, $profID, '$mark',CURRENT_TIMESTAMP);";
-				if($mysql->query($query)) {
-					$output = array('ID' => $mysql->insert_id, 'studentID' => $studentID, 'programID' => $programID, 'profID' => $profID, 'mark' => $mark);
-				} 			
-				else 	{
-					throw403();
-				};
-			break;
+﻿<?php 
+switch ($data['type']){
+	case 'add': 												// добавление
+		/* Блок взятия переменных из data*/
+		$mark = check_str($data['mark']);
+		$studentID = checkInt($data['studentID']);			
+		$programID = checkInt(data['programID']);
+		$profID =  checkInt($data['profID']);
+		/* ////////////////// */
+		$query = "-- Добавление оценки
+			INSERT INTO `StudentResults` (`Students_ID`, `Programs_ID`, `Profs_ID`, `Mark`,`Modified`)
+			VALUES ($studentID, $programID, $profID, '$mark',CURRENT_TIMESTAMP);"; 			// запрос на добавление
+		if($mysql->query($query)) {
+			$output = array('ID' => $mysql->insert_id, 'studentID' => $studentID, 'programID' => $programID, 'profID' => $profID, 'mark' => $mark);
+		} 			
+		else {
+			throw403(); 										// Ошибка
+		}
+		break;
 		
-		case 'list':
-			$studentID = checkInt($data['studentID']);
-			$query = "SELECT `ID`, `Programs_ID`, `Profs_ID`, `Mark` 
-					FROM `StudentResults` 
-					WHERE `Students_ID` = $studentID;";
-			if ($result = $mysql->query($query)) {	
-					$output = array();
-					while ($row = $result->fetch_row()) {
-					$output[] = array(
+	case 'list':
+		$studentID = checkInt($data['studentID']);
+		$query = "SELECT `ID`, `Programs_ID`, `Profs_ID`, `Mark` 
+			FROM `StudentResults` 
+			WHERE `Students_ID` = $studentID;"; // запрос на показать 
+		if ($result = $mysql->query($query)) {	
+			$output = array();
+			while ($row = $result->fetch_row()) {
+				$output[] = array(
 					'id' => $row[0],
 					'programID' => $row[1] ,
 					'profID' => $row[2],
 					'mark' => $row[3]
 					);		
-			}
-					 /* очищаем результирующий набор */
-				$result->close();		
-			} else {
-				throw403();
-			} 	
-			break;	
+			}	
+		} else {
+			throw403();
+		} 	
+		break;	
 		
-		case 'delete':
-			$noteID = checkInt($data['noteID']);
-			$query = "DELETE FROM `StudentResults` WHERE `ID` = $noteID;
+	case 'delete':
+		$noteID = checkInt($data['noteID']);
+		$query = "DELETE FROM `StudentResults` WHERE `ID` = $noteID;
 			INSERT INTO `dellog` (`Text`, `ID`) VALUES ('mark', $noteID);";
-				runmultiquery($query);
-				else {
-					$output = array('ID' =>$noteID);
-				};	
-			break; 
+		runMultiQuery($query); // запуск мультизапроса
+		$output = array('ID' =>$noteID);	
+		break; 
 		}
 		?>
